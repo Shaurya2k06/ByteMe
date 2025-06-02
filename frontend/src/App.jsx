1c2a3a2cb1489cc614eaaebb1694cced28197908
Soundcreates <shantanav7@gmail.com>
2025-06-02 20:56:13 +0530

    Added UI component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
+import { TypewriterEffectDemo } from "./components/typewriterEffect.jsx";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="w-screen h-screen bg-zinc-900">
+      <h1 className="text-stone-200">Hello</h1>
+      <TypewriterEffectDemo />
+    </div>
+  );
 }
 
 export default App;
