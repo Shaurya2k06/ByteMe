aff5d674f7cef6d5e77f818a3b5db61fedabb427
Soundcreates <shantanav7@gmail.com>
2025-06-18 19:35:03 +0530

    Fixed protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router";
+import { Routes, Route, BrowserRouter, Outlet } from "react-router";
 import LandingPage from "./LandingPage.jsx";
 import { TypewriterEffectDemo } from "./components/typewriterEffect.jsx";
 import Login from "./Login.jsx";
@@ -13,24 +13,31 @@ import QrPage from "./QrPage";
 import ScanQrPage from "./ScanPage.jsx";
 import About from "./About.jsx";
 import FingerprintTestPage from "./FingerprintTestPage";
+import ProtectedRoutes from "./context/ProtectedRoutes.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/studentDashboard" element={<StudentDashboard />} />
-        <Route path="/generate-qr" element={<QrPage />} />
-        <Route path="/scan" element={<ScanQrPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/fingerprint" element={<FingerprintTestPage />} />
+      <AuthProvider>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/about" element={<About />} />
+          <Route element={<ProtectedRoutes />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/events" element={<Events />} />
+            <Route path="/studentDashboard" element={<StudentDashboard />} />
+            <Route path="/generate-qr" element={<QrPage />} />
+            <Route path="/scan" element={<ScanQrPage />} />
 
-        <Route path="/shop" element={<Shop />} />
-      </Routes>
+            <Route path="/fingerprint" element={<FingerprintTestPage />} />
+
+            <Route path="/shop" element={<Shop />} />
+          </Route>
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
